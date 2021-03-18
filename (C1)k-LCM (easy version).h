9e5317f516fd0b40c16f9933f0d65f16a9a61bac
Sajal Gupta <sajal07091999@gmail.com>
2021-03-18 12:37:57 +0530

    Codeforces Round #708 (Div. 2)

---
+++ (C1)k-LCM (easy version).h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>> t;
+    while(t-->0){
+	  int n,k;
+	  cin>>n>>k;
+	  if(n%4==0){
+	      int n1=n/2;
+	      int n2=n1/2;
+	      std::cout << n1<<" "<<n2<<" "<<n2 << std::endl;
+	  }
+	  else if(n%2==0){n-=2;
+	      int n1=n/2;
+	      std::cout <<"2 "<<n1<<" "<<n1 << std::endl;
+	  }
+	  else{
+	      n--;
+	      int n1=n/2;
+	      std::cout <<"1 "<<n1<<" "<<n1 << std::endl;
+	  }     
+	}
+	return 0;
+}
